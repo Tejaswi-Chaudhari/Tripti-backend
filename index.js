158e24d22a1a6c9a56a6620b43466793eed97bd9
Tejaswi-Chaudhari <tejschaudhari522002@gmail.com>
2021-10-12 21:07:15 +0530

    launch-7

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express"
 import cors from "cors"
 import mongoose from "mongoose"
 import path from 'path'
-import { __dirname } from 'path';
+// import { __dirname } from 'path';
 import { } from 'dotenv/config'
 
 import bcryptjs from "bcryptjs"
