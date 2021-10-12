eacc7de4699dbd3fcef8e17bdcf82197241226a6
Tejaswi-Chaudhari <tejschaudhari522002@gmail.com>
2021-10-12 20:09:13 +0530

    launch-3

--- index.js
+++ index.js
@@ -193,7 +193,7 @@ app.post("/Treatb", (req, res) => {
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('frontend/build'));
 
-    // const path = require('path');
+    const path = require('path');
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
     });
